d99e0a030aeba590f9a4cddc35bebf7d1dfe9241
Pavel Gribachev <PGribachev@yandex.ru>
2023-11-17 21:43:43 +0300

    development of application logic, setting up elements and creating functions

--- RGBWave/ViewController.swift
+++ RGBWave/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var presentView: UIView!
+    @IBOutlet var progressLabels: [UILabel]!
+    @IBOutlet var colorRGBSliders: [UISlider]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        customizingSliders()
+        presentView.layer.cornerRadius = 10
+        
+        sliderProgressInLabel()
     }
-
-
+    
+    @IBAction func slidersRGBAction() {
+        sliderProgressInLabel()
+                
+        presentView.backgroundColor = UIColor(
+            red: CGFloat(colorRGBSliders[0].value),
+            green: CGFloat(colorRGBSliders[1].value),
+            blue: CGFloat(colorRGBSliders[2].value),
+            alpha: 1
+        )
+    }
+    
+    private func roundFloat(_ value: Float) -> String {
+        String(format:"%.02f", value)
+    }
+    
+    private func sliderProgressInLabel() {
+        progressLabels[0].text = roundFloat(colorRGBSliders[0].value)
+        progressLabels[1].text = roundFloat(colorRGBSliders[1].value)
+        progressLabels[2].text = roundFloat(colorRGBSliders[2].value)
+    }
+    
+    private func customizingSliders() {
+        colorRGBSliders.forEach { $0.minimumValue = 0 }
+        colorRGBSliders.forEach { $0.maximumValue = 1 }
+        colorRGBSliders.forEach { $0.value = 0 }
+        
+        colorRGBSliders[0].minimumTrackTintColor = .systemRed
+        colorRGBSliders[1].minimumTrackTintColor = .systemGreen
+        colorRGBSliders[2].minimumTrackTintColor = .systemBlue
+    }
 }
 
