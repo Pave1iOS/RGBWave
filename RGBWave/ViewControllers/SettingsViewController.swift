f9b137bab4542f9adc0dcf7138785220065e7983
Pavel Gribachev <PGribachev@yandex.ru>
2023-12-07 20:32:47 +0300

    improvement of functionality

--- RGBWave/ViewControllers/SettingsViewController.swift
+++ RGBWave/ViewControllers/SettingsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 final class SettingsViewController: UIViewController {
     
+    // MARK: IBOutlets
     @IBOutlet var colorView: UIView!
 
     @IBOutlet var redLabel: UILabel!
@@ -20,27 +21,39 @@ final class SettingsViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    // MARK: Properties
     weak var delegate: SettingsViewControllerDelegate?
     
+    var redColorSlider: Float!
+    var greenColorSlider: Float!
+    var blueColorSlider: Float!
+    var presentView: UIColor?
+    
+    // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorView.layer.cornerRadius = 15
         setColor()
         
+        redSlider.value = redColorSlider
+        greenSlider.value = greenColorSlider
+        blueSlider.value = blueColorSlider
+        colorView.backgroundColor = presentView
+        
         redLabel.text = string(from: redSlider)
         greenLabel.text = string(from: greenSlider)
         blueLabel.text = string(from: blueSlider)
-        
-        
     }
     
+    // MARK: IBAction
     @IBAction func doneButtonPressed() {
         delegate?.setBackgraund(
             red: redSlider.value.cgFloat(),
             green: greenSlider.value.cgFloat(),
             blue: blueSlider.value.cgFloat()
         )
+        
         dismiss(animated: true)
     }
     
@@ -58,6 +71,7 @@ final class SettingsViewController: UIViewController {
         }
     }
     
+    // MARK: Private func
     private func setColor() {
         colorView.backgroundColor = UIColor(
             red: redSlider.value.cgFloat(),
--- RGBWave/ViewControllers/StartViewController.swift
+++ RGBWave/ViewControllers/StartViewController.swift
@@ -15,18 +15,38 @@ final class StartViewController: UIViewController {
 
     weak var delegate: SettingsViewControllerDelegate?
     
+    var redColor: CGFloat!
+    var greenColor: CGFloat!
+    var blueColor: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redColor = 0.2
+        greenColor = 0.8
+        blueColor = 0.5
+        
+        view.backgroundColor = UIColor(
+            red: redColor,
+            green: greenColor,
+            blue: blueColor,
+            alpha: 1
+        )
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let settingsVC = segue.destination as? SettingsViewController
+        settingsVC?.redColorSlider = Float(redColor)
+        settingsVC?.greenColorSlider = Float(greenColor)
+        settingsVC?.blueColorSlider = Float(blueColor)
+        settingsVC?.presentView = view.backgroundColor
         
         settingsVC?.delegate = self
     }
     
 }
 
+// MARK: SettingsViewControllerDelegate
 extension StartViewController: SettingsViewControllerDelegate {
     func setBackgraund(red: CGFloat, green: CGFloat, blue: CGFloat) {
         view.backgroundColor = UIColor(
