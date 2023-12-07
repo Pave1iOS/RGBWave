8646d06f871db9955a44bc2fa6e89391616fca17
Pavel Gribachev <PGribachev@yandex.ru>
2023-12-07 17:33:22 +0300

    project setup

---
+++ RGBWave/ViewControllers/StartViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StartViewController.swift
+//  RGBWave
+//
+//  Created by Pavel Gribachev on 07.12.2023.
+//
+
+import UIKit
+
+final class StartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
